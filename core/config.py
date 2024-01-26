ee858b40495d9c3c8a13a9b5ee76747f940cad11
Dmytro <dashkin.dima@rl.odessa.ua>
2024-01-26 10:42:12 +0200

    Fixed db echo

--- core/config.py
+++ core/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     db_url: str = "sqlite+aiosqlite:///./db.sqlite3"
-    db_echo: bool = True
+    db_echo: bool = False
     jwt_secret: str = os.getenv("PRIVATE_KEY")
     jwt_algo: str = "HS256"
     jwt_expire: int = 60
